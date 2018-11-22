be176144c3a0fe8f2cf0fc29a7f095de143339bf
Lina <linamattsson@live.se>
2018-11-22 15:47:32 +0100

    Added a highscore in javafx

---
+++ src/main/java/entities/HighScore.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class HighScore {
+    private String position;
+    private String name;
+    private String points;
+
+    public HighScore(String position, String name, String points){
+        this.position = position;
+        this.name = name;
+        this.points = points;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+}
